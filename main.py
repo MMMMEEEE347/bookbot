53c5c814451163fb3db255708aed146bf6e9f99f
MMMMEEEE347 <therandomt2.0@gmail.com>
2024-11-15 13:09:39 -0600

    main.py get_word_count is now more eficent and main prints results in a more readable way

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    word_count = get_word_count(book_path)
+    word_count = get_word_count(text)
     print(text)
-    print(word_count)
+    print()
+    print(f"{word_count} words found in the document")
 
 
 def get_book_text(path):
@@ -11,10 +12,8 @@ def get_book_text(path):
         return f.read()
 
 
-def get_word_count(path):
-    with open(path) as f:
-        text = f.read()
-        words = text.split()
-        return len(words)
+def get_word_count(text):
+    words = text.split()
+    return len(words)
 
 main()
